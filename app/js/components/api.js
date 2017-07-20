ccd902519e226be1d6182ee0717e644fd3a8d122
NIKIT <nikita2008-101@yandex.ru>
2017-07-21 00:39:44 +0300

    новая таблица и api 4

--- app/js/components/api.js
+++ app/js/components/api.js
@@ -2,18 +2,34 @@
  * Модуль работы с гугл таблицами
  */
 $(function googleApi() {
-	class API{
-		constructor(tableViewer,url) {
-			this.url = url;
+	
+	class GoogleSpreadsheet{
+		constructor(key, spreadsheetId){
+			this.key = key;
+			this.spreadsheetId = spreadsheetId;
+		}
+		load(sheet,range,fun){
+			const googleUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheet}!${range}?key=${key}`;
+			console.log("get:",googleUrl);
+			$.getJSON( googleUrl, fun);
+		}
+	}
+	class API {
+		constructor(tableViewer,googleSpreadsheet) {
 			this.tableViewer = tableViewer;
-			this.googleSpreadsheet = new GoogleSpreadsheet();
-			if (localStorage.length==0 || localStorage.getItem(0)==null || localStorage.getItem(1)==null){
+			this.googleSpreadsheet = googleSpreadsheet;
+			if (localStorage.length == 0 || localStorage.getItem(0) == null || localStorage.getItem(1) == null) {
 				this.saveToLocalStorage();
 				console.log("first saveToLocalStorage()")
 			} else {
 				console.log("first showTimetable()")
-				this.showTimetable();
-				if(navigator.onLine) {
+				try{
+					this.showTimetable();
+				} catch (err) {
+					console.error("showTimetable: ",err);
+				}
+				
+				if (navigator.onLine) {
 					console.log("onLine updateStorage()")
 					this.updateStorage();
 				};
@@ -24,153 +40,163 @@ $(function googleApi() {
 		 * @param {string} url Адрес листа таблицы
 		 * @param {function} fun Что с ней делает
 		 */
-		googleSpreadsheetLoad(url,fun){
-			this.googleSpreadsheet.url(url);
-			this.googleSpreadsheet.load(function (result) {
-				console.log(`Загружен обьект:`,result);
+		googleSpreadsheetLoad(sheet,range, fun) {
+			this.googleSpreadsheet.load(sheet,range,function (result) {
+				console.log(`Загружен обьект:`, result);
 				fun(result);
 			});
 		}
 		/**
 		 * Сохранение расписания в локальное хранилище
 		 */
-		saveToLocalStorage(){
-			this.googleSpreadsheetLoad(url,(result)=>{
+		saveToLocalStorage() {
+			this.googleSpreadsheetLoad("To Technopolis","A1:H100", (result) => {
 				console.log(result)
-				localStorage.setItem(0,JSON.stringify(result));
+				localStorage.setItem(0, JSON.stringify(result));
 				this.tableViewer.addTableList(result, 0);
 			});
-			this.googleSpreadsheetLoad(url + "&gid=1453141125",(result)=>{
-				localStorage.setItem(1,JSON.stringify(result));
+			this.googleSpreadsheetLoad("From Technopolis","A1:H100", (result) => {
+				localStorage.setItem(1, JSON.stringify(result));
 				this.tableViewer.addTableList(result, 1);
 			});
-			this.googleSpreadsheetLoad(url + "&gid=135110459",(result)=>{
+			this.googleSpreadsheetLoad("Объявления","A1:B10", (result) => {
 				this.tableViewer.addInfoList(result);
 			});
-			localStorage.setItem('DATE',Date());
+			localStorage.setItem('DATE', Date());
 		}
 		/**
 		 * Обновление
 		 */
-		updateStorage(){
+		updateStorage() {
 			this.tableViewer.cleanTableList();
 			this.saveToLocalStorage();
 		}
-		showTimetable(){
+		showTimetable() {
 			this.tableViewer.addTableList(JSON.parse(localStorage.getItem(0)), 0);
 			this.tableViewer.addTableList(JSON.parse(localStorage.getItem(1)), 1);
 		}
 	}
 	class TableViewer {
-		constructor(timetableRowClass,infoClass) {
-			this.timetableRowClass=timetableRowClass;
+		constructor(timetableRowClass, infoClass) {
+			this.timetableRowClass = timetableRowClass;
 			this.infoClass = infoClass;
 		}
 		/**
-	 * Добавляет информанию из таблицы
-	 * @param {object}   infoJSON  Обьект
-	 * @param {string} infoClass Класс в который добавляем
-	 */
-		addInfoList(infoJSON){
-		console.log(`f: addTableList(infoJSON=`,
-					infoJSON,
-					`, infoClass= ${this.infoClass})`);
-		let infoHtmlString = "";
-		infoJSON.items.forEach(function (msg) {
-			infoHtmlString+= `<p class="info-list--elem">
-									<span class="info-list--elem__title">
-										${msg.id}
-									</span>
-									${msg.info}
-								</p>`;
-		});
-		$(this.infoClass).append(infoHtmlString);
-	}
-		/**
-	 * Add table list in timetable
-	 * @param   {object} timetableJSON     Data
-	 * @param   {string} timetableRowClass Table DOM class
-	 * @param   {number} id                Table list number
-	 */
-		addTableList(timetableJSON, id) {
-		console.log(`f: addTableList(id=${id}, timetableJSON=`,
-					timetableJSON,
-					`, timetableRowClass= ${this.timetableRowClass})`);
-		const minMap = {
-			"b": "00",
-			"c": "05",
-			"d": 10,
-			"e": 15,
-			"f": 20,
-			"g": 25,
-			"h": 30,
-			"i": 35,
-			"j": 40,
-			"k": 45,
-			"l": 50,
-			"m": 55
-		};
-		let timetableRowHtmlString = `<div class="timetable__row__current" id="route${id}">`;
-		timetableJSON.items.forEach(function (mins) {
-			let minHtmlString = getMinHtmlString(mins);
-			if (minHtmlString !== "") {
-				timetableRowHtmlString += `<div class="timetable__row">
-								<div class="timetable__hover">
-									${mins.id}	
-								</div>
-								<hr class="line"/>
-								<div class="timetable__row--subrow">
-									${minHtmlString}
-								</div>
-							</div>`;
-			}
-		});
-		timetableRowHtmlString += '</div>';
-		
-		/**
-		 * Добавляет строку минут в таблицу
-		 * @param   {object} mins Массив пар (Минуты:День,Id:Часы)
-		 * @returns {string} Строковое представление в таблице
+		 * Добавляет информанию из таблицы
+		 * @param {object}   infoJSON  Обьект
+		 * @param {string} infoClass Класс в который добавляем
 		 */
-		function getMinHtmlString(mins) {
-			let minsHtmlString = "";
-			for (let min in mins) {
-				if (minMap[min] == undefined) continue;
-				minsHtmlString += `<div  class="timetable__min ${getDayClassName(mins[min])}">
-								${mins.id}:${minMap[min]}
-							</div>`;
-			}
-			return minsHtmlString;
-			/**
-			 * Добавляет класс дня недели
-			 * @param   {string} tableDayName Название дня недели в таблице exel
-			 * @returns {string}   Класс дня недели
-			 */
-			function getDayClassName(tableDayName){
-				const daysInTable = ['Su', 'Mo', 'Tu', 'Te', 'Th', 'Fr', 'Sa','All'];
-				let daysExtendtion = [...days,"All"];
-				if(daysInTable.indexOf(tableDayName)===undefined){
-					console.error(`tableDayName is undefined, 
-									cur: ${tableDayName} 
-									exp: ${daysExtendtion}
-									`);
-					return ' ';
-				}
-				return ' day-'+daysExtendtion[daysInTable.indexOf(tableDayName)];
-			}
+		addInfoList(infoJSON) {
+			console.log(`f: addTableList(infoJSON=`,
+				infoJSON,
+				`, infoClass= ${this.infoClass})`);
+			let infoHtmlString = "";
+			infoJSON.values.shift();
+			infoJSON.values.forEach(function (msg) {
+				infoHtmlString += `<p class="info-list--elem">
+										<span class="info-list--elem__title">
+											${msg[0]}
+										</span>
+										${msg[1]}
+									</p>`;
+			});
+			$(this.infoClass).append(infoHtmlString);
 		}
-		
-		$(this.timetableRowClass).append(timetableRowHtmlString);
-	}
-		cleanTableList(){
+		/**
+		 * Add table list in timetable
+		 * @param   {object} timetableJSON     Data
+		 * @param   {string} timetableRowClass Table DOM class
+		 * @param   {number} id                Table list number
+		 */
+		addTableList(timetableJSON, id) {
+			console.log(`f: addTableList(id=${id}, timetableJSON=`,
+				timetableJSON,
+				`, timetableRowClass= ${this.timetableRowClass})`);
+			let timetableRowHtmlString = `<div class="timetable__row__current" id="route${id}">`;
+			//console.log(JSON.stringify(timetableJSON.values));
+			let timetable = timetableJSON.values;
+			let timeSort = {};
+			timetable.shift();
+			timetable.forEach(function (mins,i) {
+				let [hour,min] = mins[0].split(':');
+				mins.shift();
+				if(timeSort[hour]==undefined) timeSort[hour] = [];
+				timeSort[hour].push({min,mins});
+			});
+			function compareHour(a, b) {
+			  if (a.hour > b.hour) return 1;
+			  if (a.hour < b.hour) return -1;
+			}
+			function compareMin(a, b) {
+			  if (a.min > b.min) return 1;
+			  if (a.min < b.min) return -1;
+			}
+			//timeSort = timeSort.sort(compareHour);
+			for(let i=0;i<timeSort.length;i++){
+				timeSort[i] = timeSort[i].sort(compareMin);
+			}
+			//console.log(JSON.stringify(timeSort));
+			for(hour in timeSort){
+				let minsHtmlString = getMinsHtmlString(hour,timeSort[hour]);
+				if (minsHtmlString !== "") {
+					timetableRowHtmlString += `<div class="timetable__row">
+									<div class="timetable__hover">
+										${hour}	
+									</div>
+									<hr class="line"/>
+									<div class="timetable__row--subrow">
+										${minsHtmlString}
+									</div>
+								</div>`;
+				}
+			}
+			timetableRowHtmlString += '</div>';
+
+			/**
+			 * Добавляет строку минут в таблицу
+			 * @param   {object} mins Массив пар (Минуты:День,Id:Часы)
+			 * @returns {string} Строковое представление в таблице
+			 */
+			function getMinsHtmlString(hour,mins) {
+				let minsHtmlString = "";
+				mins.forEach(function (min,i) {
+						minsHtmlString += `<div  class="timetable__min ${getDayClassName(min.mins)}">
+									${hour}:${min.min}
+								</div>`;
+				});
+				return minsHtmlString;
+				/**
+				 * Добавляет класс дня недели
+				 * @param   {string} tableDayName Название дня недели в таблице exel
+				 * @returns {string}   Класс дня недели
+				 */
+				function getDayClassName(tableDayName) {
+					//console.log(tableDayName);
+					let minDaysClassNames = "";
+					let daysExtendtion = [...days, "All"];
+					for(let i=0;i<tableDayName.length;i++){
+						if(tableDayName[i]=="")  continue;
+						minDaysClassNames += ' day-' + daysExtendtion[i];
+					}
+					if(tableDayName.length==5)minDaysClassNames+= ' day-All';
+					return minDaysClassNames;
+				}
+			}
+
+			$(this.timetableRowClass).append(timetableRowHtmlString);
+		}
+		cleanTableList() {
 			$(this.timetableRowClass).html("");
 		}
-		cleanInfoList(){
+		cleanInfoList() {
 			$(this.infoClass).html("");
 		}
 	};
 	let url = "https://docs.google.com/spreadsheet/pub?key=1VwgzSFxVRu2Z-9tvF8wimO2m3BmuW4ngcST5uGSRYRg&output=html";
-	const tableViewer = new TableViewer(".timetable__row__all",".info-list");
-	const apiObj = new API(tableViewer,url);
+	const key = "AIzaSyC6gFH_bbSeMSIzEexKIUQ3MGE1MK2mBdo";
+	const spreadsheetId = "1Xdje643qsnGXaOjU70larjh-UFTh6Bt9ii83a7t68Oo";
+	const googleSpreadsheet = new GoogleSpreadsheet(key,spreadsheetId)
+	const tableViewer = new TableViewer(".timetable__row__all", ".info-list");
+	const apiObj = new API(tableViewer,googleSpreadsheet);
 	setLanguage("ru");
 });
