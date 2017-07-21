7b42decc28c5a0c737f64a661cb03e8f0a4b0c37
Tatiana <Tangord@mail.ru>
2017-07-21 03:21:38 +0300

    add save buttons

--- app/js/common.js
+++ app/js/common.js
@@ -18,13 +18,5 @@ UpUp.start({
   'service-worker-url': '/upup.sw.min.js'
 });
 
-//    
-//    //Сохранение документа с расписанием
-//    function download(text, name, type) {
-//        var a = document.createElement("a");
-//        var file = new Blob([text], {type: type});
-//        a.href = URL.createObjectURL(file);
-//        a.download = name;
-//        a.click();
-//    }
+
     
